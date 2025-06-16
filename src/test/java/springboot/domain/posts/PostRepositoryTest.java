41ef6700d5f933eb4d4217a73590c9957785cfde
sihyun10 <hyhy200170@gmail.com>
2025-06-16 22:52:54 +0900

    Spring Data Jpa 적용 후 테스트 코드 작성

---
+++ src/test/java/springboot/domain/posts/PostRepositoryTest.java
@@ -0,0 +1,48 @@
+package springboot.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach // JUnit5 <-> @After JUnit4
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("게시글 저장 불러오기")
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("hyhy200170@gmail.com")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
