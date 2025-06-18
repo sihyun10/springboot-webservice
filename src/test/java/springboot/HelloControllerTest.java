79d289d6cd9d8e92ef4344b689a6d0c9e3590dee
sihyun10 <hyhy200170@gmail.com>
2025-06-19 03:41:27 +0900

    fix: 모든 테스트 통과시킴
    메인페이지 로딩부분은 이전에 프론트엔드 부분을 고쳐주어 수정된 내용으로 작성하니 테스트 통과되었다.
    다음으로 HelloControllerTest에서는 @MockBean을 활용해 필요한 OAuth2 관련 빈을 수동으로 모킹하고,
    실제 OAuth2 인증 흐름을 시뮬레이션하여 테스트를 수행하였다.

--- src/test/java/springboot/HelloControllerTest.java
+++ src/test/java/springboot/HelloControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,7 +15,9 @@ import springboot.web.HelloController;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
 
+@MockBean(JpaMetamodelMappingContext.class)
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
@@ -21,29 +25,23 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    // 이 테스트는 testuser 이름, USER 권한을 가진 사용자가 인증된 상태에서 /hello 요청을 보낸다고 가정함
-    @WithMockUser(username = "testuser", roles = {"USER"})
     @DisplayName("hello가 리턴된다")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
-        mvc.perform(get("/hello"))
+        mvc.perform(get("/hello").with(oauth2Login()))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
 
     @Test
-    @WithMockUser(username = "testuser", roles = {"USER"})
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
-        mvc.perform(
-                get("/hello/dto")
-                        .param("name", name)
-                        .param("amount", String.valueOf(amount)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is(name)))
-                .andExpect(jsonPath("$.amount", is(amount)));
+        mvc.perform(get("/hello/dto").with(oauth2Login()).param("name", name).param("amount", String.valueOf(amount)))
+                        .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.name", is(name)))
+                                        .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
--- src/test/java/springboot/web/IndexControllerTest.java
+++ src/test/java/springboot/web/IndexControllerTest.java
@@ -21,9 +21,8 @@ public class IndexControllerTest {
     public void 메인페이지_로딩() {
         // when
         String body = this.restTemplate.getForObject("/", String.class);
-        System.out.println(body);
 
         // then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("✏️ 게시판");
     }
 }
